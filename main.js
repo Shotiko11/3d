b0fe2840fbd05bb3bbdd5a1ca685aaeba640a82f
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-10-03 11:42:33 +0400

    next commit

--- main.js
+++ main.js
@@ -30,14 +30,45 @@ cube.add(outline);
 
 camera.position.z = 5;
 
+let isDragging = false;
+let previousMousePosition = {
+  x: 0,
+  y: 0
+};
+
+// Add event listeners for mouse interactions
+document.addEventListener('mousedown', (event) => {
+  isDragging = true;
+  previousMousePosition = {
+    x: event.clientX,
+    y: event.clientY
+  };
+});
+
+document.addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+document.addEventListener('mousemove', (event) => {
+  if (isDragging) {
+    const deltaMove = {
+      x: event.clientX - previousMousePosition.x,
+      y: event.clientY - previousMousePosition.y
+    };
+
+    // Rotate the cube based on mouse movement
+    cube.rotation.x += deltaMove.y * 0.01;
+    cube.rotation.y += deltaMove.x * 0.01;
+
+    previousMousePosition = {
+      x: event.clientX,
+      y: event.clientY
+    };
+  }
+});
+
 function animate() {
   requestAnimationFrame(animate);
-
-  // Rotate the main cube
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
   renderer.render(scene, camera);
 }
 animate();
-
